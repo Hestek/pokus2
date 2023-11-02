122f1d74884851fd5f5e4fad31d0935d873fcc6b
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2023-11-02 13:35:00 +0100

    Přidejte soubory projektu.

---
+++ pokus2/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pokus2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        int playersize = 25;
+        Player player;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            player = new Player();
+            player.Distance = 10;
+            player.X = 50;
+            player.Y = 50;
+        }
+
+        public void DrawApp()
+        {
+            canvas.Children.Clear();
+            DrawPlayer(player);
+        }
+
+
+        public void DrawPlayer(Player player)
+        {
+            var rect = new Rectangle();
+            rect.Height = playersize;
+            rect.Width = playersize;
+            rect.Fill = player.SelectedColor;
+            Canvas.SetLeft(rect, player.X);
+            Canvas.SetTop(rect, player.Y);
+            canvas.Children.Add(rect);
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawApp();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Space:
+                    player.SelectNextColor();
+                    break;
+                    case Key.Up:
+                    player.Y = player.Y - player.Distance;
+                    break;
+                case Key.Down:
+                    player.Y = player.Y + player.Distance;
+                    break;
+                case Key.Left:
+                    player.X -= player.Distance;
+                    break;
+                case Key.Right:
+                    player.X += player.Distance;
+                    break;
+            }
+            DrawApp();
+        }
+    }
+}
---
+++ pokus2/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace pokus2
+{
+    public class Player
+    {
+        private int selectedColorIndex = 0;
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Distance { get; set; }
+
+        public List<Brush> Colors { get; set; }
+        public Brush SelectedColor { get => Colors[selectedColorIndex]; }
+
+        public Player()
+        {
+            Colors = new List<Brush>()
+            {
+                Brushes.Green,
+                Brushes.Red,
+                Brushes.Orange,
+                Brushes.Blue
+            };
+        }
+
+        public void SelectNextColor()
+        {
+            selectedColorIndex++;
+            if(selectedColorIndex >= Colors.Count)
+            {
+                selectedColorIndex = 0;
+            }
+        }
+    }
+}
